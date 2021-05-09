73dfe1e24137dda4b13a8afea4365083e15e19cc
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-05-09 12:32:14 +0530

    ADDED: Watch Service to watch particular directorry

--- src/test/java/com/fileOperations/NIOFileAPITest.java
+++ src/test/java/com/fileOperations/NIOFileAPITest.java
@@ -1,5 +1,6 @@
 package com.fileOperations;
 
+import com.filewatchservice.Java8WatchServiceExample;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,19 +9,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
-
 public class NIOFileAPITest {
     private static String HOME = System.getProperty("user.home");
     private static String PLAY_WITH_NIO = "TempPlayGround";
 
     @Test
     public void givenPath_WhenChecked_ThenConfirm() throws IOException {
+
         //Check File Exists
         Path homePath = Paths.get(HOME);
         Assert.assertTrue(Files.exists(homePath));
 
         //Delete File and Check File Not Exist
         Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+
         if(Files.exists(playPath))
             FileUtils.deleteFiles(playPath.toFile());
         Assert.assertTrue(Files.notExists(playPath));
@@ -39,11 +41,16 @@ public class NIOFileAPITest {
                 Assert.assertTrue(Files.exists(tempFile));
             }
         });
-
         //List files,Directories as well as Files with Extension
         Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
         Files.newDirectoryStream(playPath,path->path.toFile().isFile() &&
                 path.toString().startsWith("temp")).forEach(System.out::println);
     }
-}
+    @Test
+    public void givenADirectory_WhenWatched_ListsAllTheActivities() throws  IOException{
+        Path dir = Paths.get(HOME+"/"+PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
+} 
